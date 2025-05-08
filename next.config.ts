ac1d9e6602260051365d0ba2db95e4abeae2627a
Jacob <kontakt.jakub.wronski@gmail.com>
2025-05-08 18:42:33 +0200

    Add publish and next config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com'],
-  }
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
